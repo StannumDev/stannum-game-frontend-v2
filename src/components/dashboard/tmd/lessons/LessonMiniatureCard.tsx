fbfdadfc903c9d393499869e162b644b4387268a
mateolohezic <mateolohezic@gmail.com>
2024-10-30 16:43:26 -0300

    Update: hover lesson miniature

--- src/components/dashboard/tmd/lessons/LessonMiniatureCard.tsx
+++ src/components/dashboard/tmd/lessons/LessonMiniatureCard.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import { PlayIcon } from "@/icons";
 
 export const LessonMiniatureCard = () => {
     return (
-        <div className="w-full grid grid-cols-5 p-2 rounded-lg hover:bg-white/10 transition-200 cursor-pointer">
+        <div className="w-full grid grid-cols-5 p-2 rounded-lg hover:bg-white/10 transition-200 cursor-pointer group">
             <div className="col-span-2 aspect-video rounded-lg relative overflow-hidden">
+                <div className="size-full bg-black/75 flex justify-center items-center absolute top-0 left-0 z-10 opacity-0 group-hover:opacity-100 transition-200">
+                    <PlayIcon className="size-8"/>
+                </div>
                 <Image
-                    className="size-full aspect-video object-cover absolute top-0 left-0"
+                    className="size-full object-cover absolute top-0 left-0"
                     width={160}
                     height={90}
                     src={`https://image.mux.com/${process.env.NEXT_PUBLIC_MUX_PLAYBACK_ID}/thumbnail.png?width=160&height=90&time=5`}
