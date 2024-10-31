6a4a0f4fb3adde5b4e425107418875bc74685cd6
mateolohezic <mateolohezic@gmail.com>
2024-10-31 19:39:38 -0300

    Fix: main container height

--- src/components/dashboard/sidebar/BuscadorSidebarMobile.tsx
+++ src/components/dashboard/sidebar/BuscadorSidebarMobile.tsx
@@ -8,6 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { CrossIcon, SearchIcon } from "@/icons";
 import { SidebarMobileIndicator } from "@/components";
+import styles from "@/components/styles/sidebar.module.css";
 
 interface Props{
     isSearching: boolean;
@@ -64,45 +65,47 @@ export const BuscadorSidebarMobile = ({isSearching, setIsSearching}:Props) => {
                 isSearching &&
                 <Fragment>
                     <motion.div
-                        className="w-full pl-3 h-16 bg-background flex justify-center items-center fixed top-0 left-0 z-[99999999] pointer-events-auto"
+                        className={`w-full pl-3 pb-3 bg-background flex justify-center items-end fixed top-0 left-0 z-[99999999] pointer-events-auto ${styles.sidebar__search}`}
                         initial={{ y: '-100%' }}
                         animate={{ y: 0 }}
                         exit={{ y: '-100%' }}
                         transition={{ type: 'spring', bounce: 0, duration: 0.375 }}
                     >
-                        <form onSubmit={handleSubmit(onSubmit)} className="w-full bg-card rounded-lg flex flex-row-reverse justify-center items-center">
-                            <label htmlFor="search" className="sr-only">Buscar en STANNUM Game</label>
-                            <input
-                                type='search'
-                                maxLength={50}
-                                id="search"
-                                autoComplete="off"
-                                placeholder="Buscar..."
-                                className="grow h-10 peer placeholder:text-card-lighter focus-visible:placeholder:text-card-lightest"
-                                {...register("search",{
-                                    required: true,
-                                    maxLength: 50
-                                })}
-                            />
+                        <div className="w-full flex justify-center items-center">
+                            <form onSubmit={handleSubmit(onSubmit)} className="w-full bg-card rounded-lg flex flex-row-reverse justify-center items-center">
+                                <label htmlFor="search" className="sr-only">Buscar en STANNUM Game</label>
+                                <input
+                                    type='search'
+                                    maxLength={50}
+                                    id="search"
+                                    autoComplete="off"
+                                    placeholder="Buscar..."
+                                    className="grow h-10 peer placeholder:text-card-lighter focus-visible:placeholder:text-card-lightest"
+                                    {...register("search",{
+                                        required: true,
+                                        maxLength: 50
+                                    })}
+                                />
+                                <motion.button
+                                    type="submit"
+                                    whileTap={{ scale: 1.25, color: 'white' }}
+                                    className="px-2 h-10 text-card-lighter peer-focus-visible:text-card-lightest flex justify-center items-center"
+                                >
+                                    <SearchIcon className="size-5 transition-200"/>
+                                </motion.button>
+                            </form>
                             <motion.button
-                                type="submit"
-                                whileTap={{ scale: 1.25, color: 'white' }}
-                                className="px-2 h-10 text-card-lighter peer-focus-visible:text-card-lightest flex justify-center items-center"
+                                type="button"
+                                onClick={() => { reset(); setIsSearching(false) }}
+                                initial={{ x: '100%', opacity: 0 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                transition={{ type: 'spring', bounce: 0 }}
+                                className="size-12 flex justify-center items-center"
                             >
-                                <SearchIcon className="size-5 transition-200"/>
+                                <span className="sr-only">Limpiar buscador</span>
+                                <CrossIcon className="size-6 text-card-lighter"/>
                             </motion.button>
-                        </form>
-                        <motion.button
-                            type="button"
-                            onClick={() => { reset(); setIsSearching(false) }}
-                            initial={{ x: '100%', opacity: 0 }}
-                            animate={{ x: 0, opacity: 1 }}
-                            transition={{ type: 'spring', bounce: 0 }}
-                            className="size-12 flex justify-center items-center"
-                        >
-                            <span className="sr-only">Limpiar buscador</span>
-                            <CrossIcon className="size-6 text-card-lighter"/>
-                        </motion.button>
+                        </div>
                     </motion.div>
                     <motion.div
                         className="w-full min-h-dvh bg-[rgba(0,0,0,0.5)] fixed top-0 left-0 z-[9999999]"
