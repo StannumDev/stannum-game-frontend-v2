a41cd466be945a650ae6255bb7c615416e389066
mateolohezic <mateolohezic@gmail.com>
2025-09-09 10:46:19 -0300

    Fix: console log error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,7 +2,9 @@
  
 import { redirect } from 'next/navigation'
 export default function Error({error, reset}: {error: Error & { digest?: string }, reset: () => void}) {
-    console.log("Error occurred:", error);
-    console.log("Reset:", reset);
+    if(process.env.NEXT_PUBLIC_ENV === 'development') {
+        console.log("Error occurred:", error);
+        console.log("Reset:", reset);
+    }
     redirect("/login");
 }
